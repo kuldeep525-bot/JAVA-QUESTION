216cf627ebb3149619f8b078e04134cf4dcc5114
kuldeep kumar <sushma97797@gmail.com>
2025-09-06 21:11:07 +0530

    exception

---
+++ Packages/q10/main.java
@@ -0,0 +1,11 @@
+import p1.q10;
+
+public class main{
+  public static void main(String st[])
+  {
+    q10 obj=new q10();
+    obj.display();
+     // Access the public field
+        System.out.println("Public field value: " + obj.a);
+  }
+}
---
+++ Packages/q10/q10.java
@@ -0,0 +1,13 @@
+// 10. Create a Java program to create a package with public class and public members to be accessed in another class.
+
+package p1;
+
+public class q10{
+
+  public int a=10;
+
+  public void display()
+  {
+    System.out.println("public method");
+  }
+}
